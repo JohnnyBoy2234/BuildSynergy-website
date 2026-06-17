b262b634c2e0fc52381c39509b83f40d13a4c027
Caleb Theron <caleb.theron1@gmail.com>
2026-06-17 14:47:31 +0200

    chore: untrack .svelte-kit build output (gitignore it)

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CNEhEvh0.js","_app/immutable/chunks/Dc4lVywf.js","_app/immutable/chunks/DpOuAeNE.js","_app/immutable/chunks/xihTtKlq.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/4.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 4;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/terms/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/4.BaG4_nrL.js","_app/immutable/chunks/Dc4lVywf.js","_app/immutable/chunks/xihTtKlq.js","_app/immutable/chunks/BuN-E9BA.js"];
-export const stylesheets = ["_app/immutable/assets/4.Du55Euvv.css"];
-export const fonts = [];
